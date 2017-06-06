ff00036d2e95e32c33346d2680d0f7bae9576864
matemo22 <matemo2204@gmail.com>
2017-06-06 20:50:31 +0700

    warna kamar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,61 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+    <div class="row text-center">
+        <table class="table table-bordered">
+            <tr>
+                <td id="id1">
+                    <div class="td">test</div>
+                </td>
+                <td id="id1">
+                    <div class="td">test</div>
+                </td>
+                <td id="id1">
+                    <div class="td">test</div>
+                </td>
+                <td id="id1">
+                    <div class="td">test</div>
+                </td>
+                <td id="id1">
+                    <div class="td">test</div>
+                </td>
+            </tr>
+            <tr>
+                <td id="id2">
+                    <div class="td">test</div>
+                </td>
+                <td id="id2">
+                    <div class="td">test</div>
+                </td>
+                <td id="id2">
+                    <div class="td">test</div>
+                </td>
+                <td id="id2">
+                    <div class="td">test</div>
+                </td>
+                <td id="id2">
+                    <div class="td">test</div>
+                </td>
+            </tr>
+            <tr>
+                <td id="id3">
+                    <div class="td">test</div>
+                </td>
+                <td id="id3">
+                    <div class="td">test</div>
+                </td>
+                <td id="id3">
+                    <div class="td">test</div>
+                </td>
+                <td id="id3">
+                    <div class="td">test</div>
+                </td>
+                <td id="id3">
+                    <div class="td">test</div>
+                </td>
+            </tr>
+        </table>
+        {{-- <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
 
@@ -11,7 +64,7 @@
                     You are logged in!
                 </div>
             </div>
-        </div>
+        </div> --}}
     </div>
 </div>
 @endsection
