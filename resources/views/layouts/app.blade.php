fab295a9428ca8de1c4065329803aca0f2ec0bef
Zephrys <alvin.valor@gmail.com>
2017-06-07 16:13:16 +0700

    push jquery
    aaa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,6 +80,8 @@
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
 
+      <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
   <script>
   $(function() {
     $( "#datepicker" ).datepicker();
