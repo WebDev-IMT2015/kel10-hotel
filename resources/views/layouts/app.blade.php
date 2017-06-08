99799065df9fe4d16bb01a3ad489c534da5d20cf
matemo22 <matemo2204@gmail.com>
2017-06-08 15:38:35 +0700

    kel10-hotel
    #susun ulang script src biar bisa logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    
 </head>
 <body>
     <div id="app">
@@ -49,8 +49,9 @@
                         <!-- Authentication Links -->
                         @if (Auth::guest())
                             <li><a href="{{ route('login') }}">Login</a></li>
-                            {{-- <li><a href="{{ route('register') }}">Register</a></li> --}}
+                            <!-- <li><a href="{{ route('register') }}">Register</a></li> -->
                         @else
+                            @if(Auth::user()->authentication == 1)
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
@@ -70,6 +71,10 @@
                                     </li>
                                 </ul>
                             </li>
+                            @else
+                            <!-- Jika Bukan Admin -->
+                            <p>Bubudesuwa</p>
+                            @endif
                         @endif
                     </ul>
                 </div>
@@ -80,17 +85,12 @@
     </div>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
     <script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 
-    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-    <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
     <script>
-        $(function()
-        {
-            $( "#datepicker" ).datepicker();
-        });
-
         $(".td").click(function(){
             $("#myModal").show();
             var txt="";
